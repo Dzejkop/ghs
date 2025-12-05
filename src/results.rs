029fc4898e43234f785447ebe72fcd8ac5001ccd
Dzejkop <jakubtrad@gmail.com>
2025-12-05 21:01:48 +0100

    remove mock reference

--- src/results.rs
+++ src/results.rs
@@ -1,11 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-pub fn mock() -> CodeResults {
-    let s = std::fs::read_to_string("resp.json").unwrap();
-
-    serde_json::from_str(&s).unwrap()
-}
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CodeResults {
     pub items: Vec<ItemResult>,
@@ -48,29 +42,4 @@ pub struct TextMatch {
 pub struct MatchSegment {
     pub indices: (usize, usize),
     pub text: String,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_resp_json() {
-        let resp_json = std::fs::read_to_string("resp.json").unwrap();
-
-        let resp: CodeResults = serde_json::from_str(&resp_json).unwrap();
-
-        for item in &resp.items {
-            println!();
-            println!("{}", item.name);
-            for text_match in &item.text_matches {
-                println!("```");
-                for line in text_match.fragment.lines() {
-                    println!("{line}");
-                }
-                println!("```");
-                println!();
-            }
-        }
-    }
 }
